7ae968d4c3ae01f6c27278f0ec59ecedef90cffd
Erik Magnusson <erik.arn.magnusson@gmail.com>
2021-11-09 17:13:21 +0100

    benchmark close to finished

--- analyze.c
+++ analyze.c
@@ -1,7 +1,8 @@
 #include "analyze.h"
 #include "algorithm.h"
 
-//#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 //
@@ -11,7 +12,7 @@
 //
 // post:
 // generate sorted list with elements [0,n-1] or [n-1,0] if reverse is true.
-/*void generateSortedList(int *d, int n, bool reverse)
+void generateSortedList(int *d, int n, bool reverse)
 {
 	for (int i = 0; i<n; i++)
 	{
@@ -22,30 +23,102 @@
 	}
 }
 
+// pre: srand() has been run at some point.
+// post: [0,maxValue]
 void generateRandomList(int *d, int n, int maxValue)
 {
-	int a;
+	for (int i = 0; i<n; i++)
+	{
+		d[i] = rand()%(maxValue+1);
+	}
 }
 
 
-//TODO: VARIABLE RANDOM RANGE
-void generateTestList(const algorithm_t a, const case_t c, int *d, int n)
+int randomIndex(int list_size)
 {
-	switch(a)
-	case bubble_sort_t: // BUBBLE SORT:
-		switch(c):
-	case best_t:
-			generateSortedList(d,n);
-	case worst_t:
-			generateSortedList(d,n,true);
-	case default:
-			int a;
+	return rand()%list_size;
+}
 
-	default:
-			//TODO: implement creation of random list
-			generateSortedList(d,n);
-			//switch		
-}*/
+// pre: d is a pre allocated buffer of length n && n>0.
+// post: generate apropriate list based on algorithm and case also returns a
+// value to search for if using a search algorithm.
+int generateTestList(const algorithm_t a, const case_t c, int *d, int n)
+{
+	int maxVal = 12;
+	int searchIndex = 0;
+	switch(a)
+	{
+		case bubble_sort_t:
+			switch(c)
+			{
+				case best_t:
+					generateSortedList(d,n,false);
+					break;
+				case worst_t:
+					generateSortedList(d,n,true);
+					break;
+				case average_t:
+					generateRandomList(d,n,maxVal);
+					break;
+			}
+			break;
+		case binary_search_t:
+			generateSortedList(d,n,false);
+			searchIndex = randomIndex(n);
+			break;
+		case linear_search_t:
+			switch(c)
+			{
+				case best_t:
+					searchIndex = 0;
+					break;
+				case worst_t:
+					searchIndex = n-1;
+				case average_t:
+					randomIndex(n);
+			}
+			generateSortedList(d,n,false);
+			break;
+		default:
+			//TODO: implement best/worst creation of list for other sort algorithms.
+			generateRandomList(d,n,maxVal);
+			break;
+	}
+	
+	return d[searchIndex];
+}
+
+double runTimedBenchmark(const algorithm_t a, int *d, int n, int v, bool* searchResult)
+{
+	// function pointers was decided against to make it possible to test both sorting and searching in a single function.
+	
+
+	
+	clock_t t = clock();
+	
+	// the overhead from the switch is assumed to be completely negligable.
+	switch(a)
+	{
+		case bubble_sort_t:
+			bubble_sort(d, n);
+			break;
+		case insertion_sort_t:
+			insertion_sort(d, n);
+			break;
+		case quick_sort_t:
+			quick_sort(d, n);
+			break;
+		case linear_search_t:
+			*searchResult = linear_search(d, n, v);
+		case binary_search_t:
+			*searchResult = binary_search(d, n, v);
+	}
+
+	clock_t t2 = clock();
+
+	return ((double)(t2-t))/CLOCKS_PER_SEC;
+
+}
 
 //
 // Public
@@ -58,14 +131,35 @@ void generateTestList(const algorithm_t a, const case_t c, int *d, int n)
 // -> table to print
 // run until time is too large?
 // fp for timer.
+//
+// run a number of benchmarks for a variable size
+//
+// TODO: verify sort/search, search for elements that do not exist 
 void benchmark(const algorithm_t a, const case_t c, result_t *buf, int n)
 {
 	int size = SIZE_START;
-	for (int i = 0; i<n; i++)
+
+	int numTests = 1;
+
+
+	for (int i = 0; i<n; i++) // changing size
 	{
+		int d[size];
 		
-		
-		size = size << 1;
+		double averageTime = 0;
+		for (int j = 0; j<numTests; j++) // multiple iterations at a given size
+		{
+			int v = generateTestList(a,c,d,size);
+			bool searchResult;
+			averageTime += runTimedBenchmark(a,d,n,v,&searchResult)/((double)numTests);
+
+			//TODO: verify result
+		}
+
+		buf[i].size = size;
+		buf[i].time = averageTime;
+
+		size *= 2;
 	}
 }
 
